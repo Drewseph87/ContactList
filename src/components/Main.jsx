626f71c0831d0cfb79f9845dba4d74af7ec0bb09
Drew Harris <drewseph87@gmail.com>
2023-05-22 19:18:34 -0600

    Started with displaying tables

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,12 +1,45 @@
-import React from 'react';
+import React, { useState } from "react";
+import dummyContacts from "../dummyData";
 
-const Main = () => {
+const Main = (props) => {
+  const [contacts, setContacts] = useState(dummyContacts);
   return (
     <div id="main">
       <div id="navbar">
         <div>Contact List</div>
+        <div>
+          {contacts.map((contact) => {
+            return contact.name;
+          })}
+        </div>
       </div>
-      <div id="container">{/* your components here */}</div>
+      <div id="container">
+        {
+          <table>
+            <tbody>
+              <tr>
+                <th>Name</th>
+                <th>Phone</th>
+                <th>Email</th>
+              </tr>
+
+              {Array.contacts.map((contacts) => {})}
+              <tr>
+                <td>Person One</td>
+                <td>000-000-0000</td>
+                <td>one@email.com</td>
+              </tr>
+
+              {/* Contact 2 */}
+              <tr>
+                <td>Person Two</td>
+                <td>111-111-1111</td>
+                <td>two@email.com</td>
+              </tr>
+            </tbody>
+          </table>
+        }
+      </div>
     </div>
   );
 };
